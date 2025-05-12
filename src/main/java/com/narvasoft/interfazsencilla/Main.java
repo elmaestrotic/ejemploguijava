66db80e44b7d94851809269e1df21d5209704101
elmaestrotic <elmaestrotic@gmail.com>
2025-05-12 08:33:44 -0500

    Ejemplo de Gui en Java

---
+++ src/main/java/com/narvasoft/interfazsencilla/Main.java
@@ -0,0 +1,10 @@
+
+package com.narvasoft.interfazsencilla;
+
+public class Main {
+    public static void main(String[] args) {
+        FrmCalc x = new FrmCalc();
+        x.setVisible(true);
+    }
+    
+}
